6c5815cddb90e1c896784b400357465f6efea9c6
Julio Jorge <66150775+JulioJorge@users.noreply.github.com>
2022-09-05 11:50:16 +0100

    Game done!

--- Components/Dice.jsx
+++ Components/Dice.jsx
@@ -1,7 +1,7 @@
 export default function Dice(props){
   const background = props.isHeld ? "#59E391" : ""
   return(
-    <div className="dice-number" onClick={() => props.handleClick(props.id)} style={{background:background}}>
+    <div className="dice-number" style={{background:background}} onClick={() => props.handleClick(props.id)}>
       <h1>{props.value}</h1>
     </div>
   )
--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,73 @@ import React from "react";
 import "./App.css"
 import Dice from "../Components/Dice";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App(){
-  const [dice, setDice] = React.useState(allNewDice())
-  const diceElements = dice.map(die => {
-    return(
-      <Dice key={die.id} id={die.id} value={die.value} isHeld={die.isHeld} handleClick={holdDice}/>
-    )
-  })
+  const [dice, setDice] = React.useState(allDice())
+  const [won, setWon] = React.useState(false)
+  
+  React.useEffect(() => {
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    const allOnHold = dice.every(die => die.isHeld)
 
-  function generateRadomNumber(){
+    if(allSameValue && allOnHold){
+      console.log("You Won")
+      setWon(true)
+    }
+  },[dice])
+
+  function generateRandomNumber(){
     return Math.ceil(Math.random()*6)
   }
 
-  function allNewDice(){
+  function allDice(){
     const dices = []
     for(let i = 0; i < 10; i++){
       dices.push({
         id: nanoid(),
-        value: generateRadomNumber(),
+        value: generateRandomNumber(),
         isHeld: false
       })
     }
     return dices
   }
-
-  function rollDice() {
-    setDice(prevDice => prevDice.map(dice => {
-      return {
-        ...dice,
-        value: generateRadomNumber()
-      }
+  function holdDice(id){
+    setDice(dice.map(die => {
+      return (id === die.id ? {
+        ...die,
+        isHeld: !die.isHeld
+      }:die)
     }))
-    console.log(dice)
   }
 
-  function holdDice(id) {
-    /**Create the logic of hold dice */
+  const diceElements = dice.map(die => {
+    return (<Dice key={die.id} id={die.id} value={die.value} isHeld={die.isHeld} handleClick={holdDice} />)
+  })
+
+  function rollDice(){
+    if(!won){
+      setDice(dice.map(die => {
+        return(die.isHeld ? die : {
+          ...die,
+          value: generateRandomNumber()
+        })
+      }))
+    }else {
+      setDice(allDice())
+      setWon(false)
+    }
   }
 
   return(
     <div className="container">
       <div className="dice-container">
         <div className="dice-wrapper">
+          {won && <Confetti />}
           {diceElements}
         </div>
-        <button className="btn" onClick={rollDice}>Roll</button>
+        <button className="btn" onClick={rollDice}>{won ? "Restart Game" : "Roll"}</button>
       </div>
     </div>
   )
